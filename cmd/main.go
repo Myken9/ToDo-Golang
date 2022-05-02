4bec8a79362ae745df0caf6c578c179c9b8f33ca
valery <ebalaika1994@gmail.com>
2022-05-02 18:35:06 +0300

    create handler's methods

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	todo "github.com/Myken9/ToDo-Golang.git"
+	"github.com/Myken9/ToDo-Golang.git/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatal(err)
 	}
 }
