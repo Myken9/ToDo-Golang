c8d8dfa5daf3ec8cc6554fe13bc0889d9ffb12c3
valery <ebalaika1994@gmail.com>
2022-05-02 18:57:25 +0300

    create repository and service

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	todo "github.com/Myken9/ToDo-Golang.git"
 	"github.com/Myken9/ToDo-Golang.git/pkg/handler"
+	"github.com/Myken9/ToDo-Golang.git/pkg/repository"
+	"github.com/Myken9/ToDo-Golang.git/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatal(err)
