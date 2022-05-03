163837baa3791749c6e4bea99ca2025ec494fa66
valery <ebalaika1994@gmail.com>
2022-05-03 10:54:01 +0300

    create db and connect PostgreSQL

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,29 @@ import (
 	"github.com/Myken9/ToDo-Golang.git/pkg/handler"
 	"github.com/Myken9/ToDo-Golang.git/pkg/repository"
 	"github.com/Myken9/ToDo-Golang.git/pkg/service"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Post:     "5432",
+		Username: "valery",
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   "tododb",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatalf("failed to initiize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
